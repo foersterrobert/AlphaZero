b40c0e9664613b14636d4ead9b62d26a08c6e2e4
robert foerster <foersteroberthh@gmail.com>
2022-12-03 12:30:50 +0100

    changed hyperparam

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
                 'c_puct': 2,                      # the value of the constant policy
                 'augment': False,                 # whether to augment the training data with flipped states
                 'dirichlet_alpha': 0.3,           # the value of the dirichlet noise
-                'dirichlet_epsilon': 0.25,        # the value of the dirichlet noise
+                'dirichlet_epsilon': 0.125,        # the value of the dirichlet noise
             }
 
             game = TicTacToe()
