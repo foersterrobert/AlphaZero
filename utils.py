dfa6e8af53c188086e017d37bbb7886cb7a2f7df
robert foerster <foersteroberthh@gmail.com>
2022-12-03 13:39:59 +0100

    temperature

--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 class KaggleAgent:
-    def __init__(self, model, game, augment=False):
+    def __init__(self, model, game, augment=False, temperature=0):
         self.model = model
         self.game = game
         self.augment = augment
+        self.temperature = temperature
 
     def run(self, obs, conf):
         player = obs['mark'] if obs['mark'] == 1 else -1
@@ -15,6 +16,15 @@ class KaggleAgent:
         valid_moves = self.game.get_valid_locations(state)
 
         policy, value = self.model.predict(state, augment=self.augment)
-        policy = policy * valid_moves
-        action = int(np.argmax(policy))
+        policy *= valid_moves
+        policy /= np.sum(policy)
+
+        if self.temperature == 0:
+            action = int(np.argmax(policy))
+
+        else:
+            policy = policy ** (1 / self.temperature)
+            policy /= np.sum(policy)
+            action = np.random.choice(self.game.action_size, p=policy)
+
         return action
